0e72c3ed4ea3956ca9c4be20842d85dd7ac3758c
Leandro Pollano <leandropollano@gmail.com>
2025-08-16 20:11:13 -0300

    initial commit

---
+++ src/main/java/com/aluracursos/LiterAlura_ChallengeONE_SpringBoot/LiterAluraChallengeOneSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.LiterAlura_ChallengeONE_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraChallengeOneSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAluraChallengeOneSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/LiterAlura_ChallengeONE_SpringBoot/LiterAluraChallengeOneSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.LiterAlura_ChallengeONE_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraChallengeOneSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
