c23cb2956e48ff95acb5fdb13621ec9fbf5c410e
Leandro Pollano <leandropollano@gmail.com>
2025-08-16 22:32:10 -0300

    Add Lógica de consumo de API

--- src/main/java/com/aluracursos/LiterAlura_ChallengeONE_SpringBoot/services/ConsumoDeApi.java
+++ src/main/java/com/aluracursos/LiterAlura_ChallengeONE_SpringBoot/services/ConsumoDeApi.java
@@ -1,4 +1,32 @@
 package com.aluracursos.LiterAlura_ChallengeONE_SpringBoot.services;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ConsumoDeApi {
+    private final HttpClient client;
+
+    public ConsumoDeApi() {
+        this.client = HttpClient.newHttpClient();
+    }
+
+    public String obtenerDatos(String url) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        } catch (IOException e) {
+            System.err.println("Error. Problemas de conexión con la API: " + e.getMessage());
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println("La solicitud fue interrumpida: " + e.getMessage());
+        }
+        return "";
+    }
 }
